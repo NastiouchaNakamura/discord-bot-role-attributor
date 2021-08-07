b48ab75e26665dd7a6d0c8ca1dc980eb141d606d
Anaël iMac <anael@barodine.fr>
2021-08-07 21:18:29 +0200

    fix: removed Main class

--- src/main/java/fr/barodine/anael/discord/roleattributor/Main.java
+++
@@ -1,9 +0,0 @@
-package fr.barodine.anael.discord.roleattributor;
-
-import fr.barodine.anael.discord.launcher.Launcher;
-
-public class Main {
-    public static void main(String[] args) {
-        Launcher.launch();
-    }
-}
